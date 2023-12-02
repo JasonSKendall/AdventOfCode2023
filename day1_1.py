9536858e75476fac1a37126dffab091adb0503f7
Jason Kendall <jasonskendall@gmail.com>
2023-12-01 20:14:15 -0500

    first commit

---
+++ day1_1.py
@@ -0,0 +1,17 @@
+#!python
+
+# https://adventofcode.com/2023/day/1
+
+import re
+
+num = 0
+with open('/Users/jasonkendall/Desktop/Advent_of_code/input.txt' , mode='r') as fhand:
+    for rawline in fhand:
+        current_line = rawline.rstrip()
+        #print(current_line)
+        code = re.sub( '[a-zA-Z]' , '' ,   current_line)
+        #print(code)
+        num += int( code[0] + code[len(code) - 1 ]  )
+        #print(num)
+print(num)
+
